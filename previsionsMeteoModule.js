e926f7f415aa97d8547a77fec98002cbf744b99c
nonoxD <noe@marleix.com>
2021-01-04 12:06:06 +0100

    index in APIs swaped with enum

--- previsionsMeteoModule.js
+++ previsionsMeteoModule.js
@@ -1,6 +1,6 @@
 //lien vers l'api :  https://www.prevision-meteo.ch/services
 //sa doc pour json : https://www.prevision-meteo.ch/uploads/pdf/recuperation-donnees-meteo.pdf
-import {giveRightTextAndIcon} from "./dictionnaries_module.js";
+import {meteo} from "./index.js";
 
 export async function previsionmeteoApi (city) {
 	const url = `https://www.prevision-meteo.ch/services/json/${city}`;
@@ -20,8 +20,7 @@ export async function previsionmeteoApi (city) {
 	windDir.innerText = data.current_condition.wnd_dir;
 	windSpeed.innerText = data.current_condition.wnd_spd;
 	const weatherCode = data.current_condition.condition;
-	let weatherCodeLocal = weatherCodePrevisionsMeteo(weatherCode);
-	let finalData = giveRightTextAndIcon(weatherCodeLocal);
+	let finalData = weatherCodePrevisionsMeteo(weatherCode);
 	weatherDesscription.innerText = finalData.weatherDescription;
 	outputCity.src = finalData.iconUrl;
 }
@@ -29,41 +28,41 @@ export async function previsionmeteoApi (city) {
 function weatherCodePrevisionsMeteo(weatherCode)
 {
 	var previsionsMeteoDictionnary = {
-		"Ensoleillé": 0,
-		"Nuit claire": 0,
-		"Pluie faible": 1,
-		"Pluie modérée": 2,
-		"Pluie forte": 3,
-		"Ciel voilé ": 4,
-		"Nuit légèrement voilée": 4,
-		"Faibles passages nuageux": 4,
-		"Stratus": 4,
-		"Stratus se dissipant": 4,
-		"Nuit claire et stratus": 4,
-		"Eclaircies": 4,
-		"Nuit nuageuse": 4,
-		"Faiblement nuageux": 4,
-		"Fortement nuageux": 4,
-		"Développement nuageux": 4,
-		"Nuit avec développement nuageux": 4,
-		"Faiblement orageux": 5,
-		"Nuit faiblement orageuse": 5,
-		"Orage modéré": 5,
-		"Fortement orageux": 5,
-		"Neige faible": 6,
-		"Neige modérée": 7,
-		"Neige forte": 8,
-		"Pluie et neige mêlée faible": 10,
-		"Pluie et neige mêlée modérée": 10,
-		"Pluie et neige mêlée forte": 10,
-		"Brouillard": 11,
-		"Averses de pluie faible": 12,
-		"Nuit avec averses": 12,
-		"Averses de pluie modérée": 12,
-		"Averses de pluie forte": 12,
-		"Couvert avec averses": 12,
-		"Averses de neige faible": 13,
-		"Nuit avec averses de neige faible": 13,
+		"Ensoleillé": meteo.sunny,
+		"Nuit claire": meteo.sunny,
+		"Pluie faible": meteo.lightRain,
+		"Pluie modérée": meteo.rain,
+		"Pluie forte": meteo.strongRain,
+		"Ciel voilé ": meteo.cloudy,
+		"Nuit légèrement voilée": meteo.cloudy,
+		"Faibles passages nuageux": meteo.cloudy,
+		"Stratus": meteo.cloudy,
+		"Stratus se dissipant": meteo.cloudy,
+		"Nuit claire et stratus": meteo.cloudy,
+		"Eclaircies": meteo.cloudy,
+		"Nuit nuageuse": meteo.cloudy,
+		"Faiblement nuageux": meteo.cloudy,
+		"Fortement nuageux": meteo.cloudy,
+		"Développement nuageux": meteo.cloudy,
+		"Nuit avec développement nuageux": meteo.cloudy,
+		"Faiblement orageux": meteo.thunder,
+		"Nuit faiblement orageuse": meteo.thunder,
+		"Orage modéré": meteo.thunder,
+		"Fortement orageux": meteo.thunder,
+		"Neige faible": meteo.lightSnow,
+		"Neige modérée": meteo.snow,
+		"Neige forte": meteo.strongSnow,
+		"Pluie et neige mêlée faible": meteo.rainAndSnow,
+		"Pluie et neige mêlée modérée": meteo.rainAndSnow,
+		"Pluie et neige mêlée forte": meteo.rainAndSnow,
+		"Brouillard": meteo.fog,
+		"Averses de pluie faible": meteo.downpour,
+		"Nuit avec averses": meteo.downpour,
+		"Averses de pluie modérée": meteo.downpour,
+		"Averses de pluie forte": meteo.downpour,
+		"Couvert avec averses": meteo.downpour,
+		"Averses de neige faible": meteo.downpourSnow,
+		"Nuit avec averses de neige faible": meteo.downpourSnow,
 	}
 	let weatherCodeLocal = previsionsMeteoDictionnary[weatherCode];
 	return weatherCodeLocal;
--- weatherStackApiModule.js
+++ weatherStackApiModule.js
@@ -1,4 +1,4 @@
-import {giveRightTextAndIcon} from "./dictionnaries_module.js";
+import {meteo} from "./index.js";
 
 export async function weatherstackApi(city) {
 	const accessKey = "fcece908fe429e785ee4a28314e30573";
@@ -19,39 +19,38 @@ export async function weatherstackApi(city) {
 	windDir.innerText = data.current.wind_dir;
 	windSpeed.innerText = data.current.wind_speed;
 	let weatherCode = data.current.weather_code;
-	let weatherCodeLocal = weatherCodeWeatherStack(weatherCode);
-	let finalData = giveRightTextAndIcon(weatherCodeLocal);
+	let finalData = weatherCodeWeatherStack(weatherCode);
 	weatherDesscription.innerText = finalData.weatherDescription;
 	outputCity.src = finalData.iconUrl;
 }
 
 function weatherCodeWeatherStack(weatherCode) {
 	var weatherStacDictionnary = {
-		113: 0,
-		296: 1,
-		302: 2,
-		305: 3,
-		308: 3,
-		116: 4,
-		119: 4,
-		122: 4,
-		200: 5,
-		227: 6,
-		260: 6,
-		281: 6,
-		182: 7,
-		185: 7,
-		284: 7,
-		230: 8,
-		311: 9,
-		143: 11,
-		248: 11,
-		263: 11,
-		266: 11,
-		176: 12,
-		293: 12,
-		299: 12,
-		179: 13,
+		113: meteo.sunny,
+		296: meteo.lightRain,
+		302: meteo.rain,
+		305: meteo.storngRain,
+		308: meteo.storngRain,
+		116: meteo.cloudy,
+		119: meteo.cloudy,
+		122: meteo.cloudy,
+		200: meteo.thunder,
+		227: meteo.lightSnow,
+		260: meteo.lightSnow,
+		281: meteo.lightSnow,
+		182: meteo.snow,
+		185: meteo.snow,
+		284: meteo.snow,
+		230: meteo.strongSnow,
+		311: meteo.hail,
+		143: meteo.fog,
+		248: meteo.fog,
+		263: meteo.fog,
+		266: meteo.fog,
+		176: meteo.downpour,
+		293: meteo.downpour,
+		299: meteo.downpour,
+		179: meteo.downpourSnow,
 	}
 	let weatherCodeLocal = weatherStacDictionnary[weatherCode];
 	return weatherCodeLocal;
